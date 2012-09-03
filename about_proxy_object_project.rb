0cff200ac4c24e60e4065d0c8a70dc4b7030719a
amitsuroliya <amitkumarsuroliya@gmail.com>
2012-09-03 12:36:12 +0530

    Finish Ruby Koans

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -16,9 +16,26 @@ class Proxy
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages_sent = Hash.new(0)
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+     @messages_sent[method_name] += 1
+     @object.__send__(method_name, *args, &block) 
+  end
+  
+  def messages
+    @messages_sent.keys
+  end
+
+  def called?(method_name)
+    messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages_sent[method_name]
+  end
 end
 
 # The proxy object should pass the following Koan:
